309edb367d36c5eb9369b11d71d235cab168974c
mahmoud0samir <152748722+mahmoud0samir@users.noreply.github.com>
2024-02-04 00:46:46 +0200

    Controller And Views And Viewstart with layout and some model

--- Department.cs
+++ Department.cs
@@ -1,26 +1,143 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using Portal.Core.Interface;
+using Portal.Core.Models;
+using Portal.Core.Repiostry;
+using Portal.Infastration.Database;
+using Portal.Infastration.Entity;
+using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-
-namespace Portal.Infastration.Entity
+namespace PortalDEmoooo.Controllers
 {
-    [Table("Department")]
-    public class Department
+    public class DepartmentController : Controller
     {
-        [Key]
-        public int ID { get; set; }
-        [Required ,StringLength(50)]
-        public string? Name { get; set; }
-        [Required, StringLength(50)]
 
-        public string? Code { get; set; }
+        //DepartmentRep department = new DepartmentRep();
 
-        public ICollection<Employee> Employees { get; set; }
+        private readonly IDepartmenRep department;
+        private readonly IMapper mapper;
 
+        public DepartmentController(IDepartmenRep department, IMapper mapper)
+        {
+            this.department = department;
+            this.mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var depts = await department.GetAsync();
+            var data = mapper.Map<IEnumerable<DepartmentVM>>(depts);
+
+            return View(data);
+        }
+        public async Task<IActionResult> Details(int ID)
+        {
+            var dpt = await department.GetByIdAsync(ID);
+            var data =mapper.Map<DepartmentVM>(dpt);    
+            return View(data);
+        }
+
+    
+
+  
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int ID)
+        {
+            var depts = await department.GetByIdAsync(ID);
+            var data = mapper.Map<DepartmentVM>(depts);
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(DepartmentVM model) {
+
+            try
+            {
+
+                if (ModelState.IsValid)
+                return View(model);
+
+                var data = mapper.Map<Department>(model);
+
+                if (data != null)
+                {
+                    await department.UpdateAsync(data);
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                TempData["Error"] = "";
+            }
+            return RedirectToAction("Index", "Department");
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int ID)
+        {
+            var depts = await department.GetByIdAsync(ID);
+            var data = mapper.Map<DepartmentVM>(depts);
+
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(DepartmentVM model)
+        {
+
+            try
+            {
+
+
+                var data = mapper.Map<Department>(model);
+
+                await department.DeleteAsync(data);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                TempData["Error"] = "";
+            }
+            return RedirectToAction("Index", "Department");
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(DepartmentVM model)
+        {
+
+            try
+            {
+
+                if (!ModelState.IsValid)
+                    return View(model);
+
+
+                var data = mapper.Map<Department>(model); 
+                    await department.CreatAsync(data);
+
+                
+
+
+
+            }
+            catch (Exception)
+            {
+
+                TempData["Error"] = "";
+            }
+            return RedirectToAction("Index", "Department");
+
+        }
     }
 }
---
+++ EmployeeController.cs
@@ -0,0 +1,149 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Portal.Core.Interface;
+using Portal.Core.Models;
+using Portal.Infastration.Entity;
+
+namespace PortalDEmoooo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepoistory employee;
+        private readonly IDepartmenRep department;
+        private readonly IMapper mapper;
+
+        public EmployeeController(IEmployeeRepoistory employee , IDepartmenRep department ,IMapper mapper)
+        {
+            this.employee = employee;
+            this.department = department;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var emps = await employee.GetAsync(emp =>emp.IsDeleted ==false && emp.IsActive == true);
+            var data = mapper.Map<IEnumerable<EmployeeVm>>(emps);
+
+            return View(data);
+        }
+        public async Task<IActionResult> Details(Guid id)
+        {
+            
+            var emp = await employee.GetByIdAsync(emp =>emp.id == id && emp.IsDeleted == false && emp.IsActive == true);
+            ViewBag.Departmentlist = new SelectList(await department.GetAsync(), "ID", "Name");
+            var data = mapper.Map<EmployeeVm>(emp);
+            return View(data);
+        }
+
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+
+            var emps = await employee.GetByIdAsync(emp => emp.id == id && emp.IsDeleted == false && emp.IsActive == true);
+            var data = mapper.Map<EmployeeVm>(emps);
+            ViewBag.Departmentlist = new SelectList(await department.GetAsync(), "ID", "Name" ,data.DepartmentID);
+
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(EmployeeVm model)
+        {
+
+            try
+            {
+                ViewBag.Departmentlist = new SelectList(await department.GetAsync(), "ID", "Name",model.DepartmentID);
+
+
+                if (ModelState.IsValid)
+                    return View(model);
+
+                var data = mapper.Map<Employee>(model);
+
+                if (data != null)
+                {
+                    await employee.UpdateAsync(data);
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                TempData["Error"] = "";
+            }
+            return RedirectToAction("Index", "Employee");
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var emps = await employee.GetByIdAsync(emp => emp.id == id && emp.IsDeleted == false && emp.IsActive == true);
+            var data = mapper.Map<EmployeeVm>(emps);
+
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(EmployeeVm model)
+        {
+
+            try
+            {
+
+
+                var data = mapper.Map<Employee>(model);
+
+                await employee.DeleteAsync(data);
+
+
+
+            }
+            catch (Exception)
+            {
+
+                TempData["Error"] = "";
+            }
+            return RedirectToAction("Index", "Employee");
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Departmentlist = new SelectList(await department.GetAsync(),"ID","Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(EmployeeVm model)
+        {
+
+            try
+            {
+                ViewBag.Departmentlist = new SelectList(await department.GetAsync(), "ID", "Name");
+
+                if (!ModelState.IsValid)
+                    return View(model);
+
+
+                var data = mapper.Map<Employee>(model);
+                await employee.CreatAsync(data);
+
+
+
+
+
+            }
+            catch (Exception)
+            {
+
+                TempData["Error"] = "";
+            }
+            return RedirectToAction("Index", "Employee");
+
+        }
+    }
+}
